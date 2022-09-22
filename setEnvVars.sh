2c3abc7559757689708a54c0b6b9d47c3c699821
Japneet Kaur <jkaur@dminc.com>
2022-09-22 14:36:36 -0400

    updated envVars

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
 export GITHUB_USERNAME="kaurjap"
-export GITHUB_REPO="bootcamp-2022-sls-api-kaur.git"
+export GITHUB_REPO="bootcamp-2022-sls-api-kaur"
 export BUCKET_NAME="kaurjap-bootcamp-2022-bucket" #username must be all lowercase
